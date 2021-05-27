a2865fa947106d0a5d34c705dbbecf548ba75f9c
Gabriel Ebner <gebner@gebner.org>
2021-05-27 18:55:53 +0200

    need more cash

--- examples/mkPairwiseEquality.lean
+++ examples/mkPairwiseEquality.lean
@@ -1,7 +1,7 @@
 import Qq
 open Qq
 
-def mkPairwiseEquality {α : Q(Sort u)} : List Q(α) → Q(Prop)
-  | [a, b] => q(a = b)
-  | a :: b :: cs => q(a = b ∧ $(mkPairwiseEquality (b :: cs)))
+def mkPairwiseEquality {α : Q(Sort u)} : List Q($α) → Q(Prop)
+  | [a, b] => q($a = $b)
+  | a :: b :: cs => q($a = $b ∧ $(mkPairwiseEquality (b :: cs)))
   | _ => q(True)
