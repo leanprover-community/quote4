5e771618711ec5cb5fd3c7fda4064d669ac9b97a
Gabriel Ebner <gebner@gebner.org>
2022-07-20 15:29:00 +0200

    Extend matching example.

--- examples/matching.lean
+++ examples/matching.lean
@@ -5,6 +5,16 @@ partial def summands : Q(Nat) → MetaM (List Q(Nat))
   | ~q($x + $y) => return (← summands x) ++ (← summands y)
   | n => return [n]
 
+opaque k : Nat
+opaque m : Nat
+
 abbrev double (a : Nat) := a + a
 
-#eval summands q(double 42 + 7)
+#eval summands q(double k + m)
+
+abbrev square (a : Nat) :=
+  have : Add Nat := ⟨(· * ·)⟩
+  a + a
+
+#eval square 10
+#eval summands q(square k + m)
