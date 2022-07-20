37480718c0580d298d4f29c7e93f204c218eb133
Gabriel Ebner <gebner@gebner.org>
2022-07-20 16:43:42 +0200

    Use standard code style.

--- examples/betterApp.lean
+++ examples/betterApp.lean
@@ -8,7 +8,7 @@ set_option trace.compiler.ir.result true in
 -- `autoBoundImplicitLocal`.
 
 def betterApp {α : Q(Sort u)} {β : Q($α → Sort v)}
-  (f : Q((a : $α) → $β a)) (a : Q($α)) : Q($β $a) :=
-q($f $a)
+    (f : Q((a : $α) → $β a)) (a : Q($α)) : Q($β $a) :=
+  q($f $a)
 
 #eval betterApp q(Int.toNat) q(42)
