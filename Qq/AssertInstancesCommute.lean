b7c3ebf075ba965c8d23969821d33c74aef03456
Gabriel Ebner <gebner@gebner.org>
2023-07-14 20:55:16 -0700

    Term-mode assumeInstancesCommute

--- Qq/AssertInstancesCommute.lean
+++ Qq/AssertInstancesCommute.lean
@@ -2,6 +2,8 @@ import Qq.MetaM
 
 namespace Qq
 
+scoped syntax "assumeInstancesCommute" term : term
+
 namespace Impl
 open Lean Meta Elab Term
 
@@ -22,7 +24,13 @@ def findRedundantLocalInst? : QuoteM (Option (FVarId × Expr)) := do
   return none
 
 def findRedundantLocalInstQuoted? :
-    MetaM (Option (FVarId × (u : Q(Level)) × (ty : Q(QQ (mkSort $u))) × Q(QQ $ty) × Q(QQ $ty))) := do
+    TermElabM (Option (FVarId × (u : Q(Level)) × (ty : Q(QQ (mkSort $u))) × Q(QQ $ty) × Q(QQ $ty))) := do
+  for ldecl in ← getLCtx do
+    let ty ← whnfR ldecl.type
+    if ty.isMVar then tryPostpone
+    if ty.isAppOf ``QQ then
+      if (← instantiateMVars ty.appArg!).hasExprMVar then
+        tryPostpone
   StateT.run' (m := MetaM) (s := {}) do
   unquoteLCtx
   (← findRedundantLocalInst?).mapM fun (fvar, inst) => do
@@ -43,15 +51,14 @@ elab_rules : term <= expectedType | `(assertInstancesCommuteImpl $cont) => do
       expectedType
   | none => elabTerm cont expectedType
 
-scoped syntax "assumeInstancesCommuteImpl" term : term
-elab_rules : term <= expectedType | `(assumeInstancesCommuteImpl $cont) => do
+elab_rules : term <= expectedType | `(assumeInstancesCommute $cont) => do
   match ← findRedundantLocalInstQuoted? with
   | some ⟨fvar, _, _, lhs, rhs⟩ =>
     let n ← mkFreshUserName ((← fvar.getUserName).eraseMacroScopes.appendAfter "_eq")
     let ty := q(QE $lhs $rhs)
     elabTerm (← `(
         have $(mkIdent n) : $(← exprToSyntax ty) := ⟨⟩
-        assumeInstancesCommuteImpl $cont))
+        assumeInstancesCommute $cont))
       expectedType
   | none => elabTerm cont expectedType
 
@@ -60,10 +67,10 @@ macro_rules
   | `(assert! assertInstancesCommuteDummy; $cont) =>
     `(assertInstancesCommuteImpl $cont)
 
-scoped syntax "assumeInstancesCommuteDummy" : term
+syntax "assumeInstancesCommuteDummy" : term
 macro_rules
   | `(assert! assumeInstancesCommuteDummy; $cont) =>
-    `(assumeInstancesCommuteImpl $cont)
+    `(assumeInstancesCommute $cont)
 
 end Impl
 open Impl
