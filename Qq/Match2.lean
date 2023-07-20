3d46be8d3a9176dbd42051fe1220e94e99aef91c
Gabriel Ebner <gebner@gebner.org>
2023-07-19 22:40:06 -0700

    wip

--- Qq/Match2.lean
+++ Qq/Match2.lean
@@ -18,9 +18,6 @@ inductive Instr
   | checkDefEqR (arg : Nat) (e : Expr)
   deriving Repr, Nonempty, BEq
 
--- @[match_pattern] def ref (idx : Nat) : Expr := .fvar ⟨.num .anonymous idx⟩
--- @[match_pattern] def refL (idx : Nat) : Level := .param (.num .anonymous idx)
-
 structure CompileState where
   varMVars : Array (Sum Expr Level) := #[]
   instrs : Array Instr := #[]
@@ -34,7 +31,7 @@ def CompileM.run (k : CompileM α) : MetaM α :=
 
 def nextVar (ty : Expr) : CompileM Nat := do
   let i := (← get).varMVars.size
-  let mvar ← mkFreshExprMVar ty
+  let mvar ← mkFreshExprMVar ty (kind := .syntheticOpaque) (userName := s!"x{i}")
   modify fun s => { s with varMVars := s.varMVars.push (.inl mvar) }
   return i
 
@@ -66,10 +63,20 @@ def applyWithMVars (e : Expr) : Nat → MetaM Expr
     let .forallE bn dom _ _ ← whnf (← inferType e) | throwError "not a function{indentExpr e}"
     applyWithMVars (e.app (← mkFreshExprMVar dom (userName := bn))) n
 
+deriving instance BEq for Sum
+
+def isPatternMVar (mvar : Expr) : CompileM Bool := do
+  let .mvar mvarId := mvar | return false
+  if (← get).varMVars.contains (.inl mvar) then return false
+  mvarId.isAssignable
+
 partial def compile (pat : Expr) (known : Expr) (idx : Nat) (whnf := false) : CompileM Unit := do
+  let pat ← instantiateMVars pat
+  let known ← instantiateMVars known
   let pat ← whnfR pat
-  if let .mvar mvarId := pat then if ← mvarId.isAssignable then
-    mvarId.assign (← ref idx)
+  logInfo m!"{← instantiateMVars known}  ===  {← instantiateMVars pat}"
+  if ← isPatternMVar pat then
+    pat.mvarId!.assign (← ref idx)
     assertDefEq known (← ref idx)
     return
   if ← withNewMCtxDepth (isDefEq pat known) then
@@ -84,21 +91,20 @@ partial def compile (pat : Expr) (known : Expr) (idx : Nat) (whnf := false) : Co
     let as' ← pat.getAppArgs.mapM fun a => do nextVar (← inferType a)
     pushInst <| .constApp idx c ls' as'.toList
     let known' ← applyWithMVars (← mkConstWithFreshMVarLevels c) as'.size
-    -- dbg_trace known'
     assertDefEq known known'
-    logInfo m!"{(← instantiateMVars known, ← instantiateMVars pat)}"
+    logInfo m!"{← instantiateMVars known}  ===  {← instantiateMVars pat}"
     for i in as', p in pat.getAppArgs, k in known'.getAppArgs do
-      if (← isClass? (← inferType p)).isSome then 
+      if (← isClass? (← inferType p)).isSome && !(← isPatternMVar (← instantiateMVars p)) then
         pushConstr <| .checkDefEqInst i p
         if ← withNewMCtxDepth do isDefEq (← inferType k) (← inferType p) then
           _ ← isDefEq k p
-        pure ()
       else
         dbg_trace p
         compile p k i
-    logInfo m!"{(← instantiateMVars known, ← instantiateMVars pat)}"
+    logInfo m!"{← instantiateMVars known}  ===  {← instantiateMVars pat}"
     return
-  if let .fvar _ := pat.getAppFn then
+  if pat.getAppFn.isFVar || (← get).varMVars.contains (.inl pat.getAppFn) then
+    logInfo (toString idx)
     pushConstr <| .checkDefEqR idx pat
     assertDefEq pat known
     return
@@ -118,8 +124,9 @@ elab "demo" pat:term : term => do
   -- logInfo _
   return known
 
+set_option pp.all true
 variable {α : Type} [Add α]
 #check demo _ + _ = (_ : α)
 #check demo (_ + _ : Nat)
 #check demo (_ + _ : α)
-#check demo (@HAdd.hAdd _ _ _ (_) _ _)
+#check demo (@HAdd.hAdd ?a ?a ?a (_) _ _)
