8cbc06ec3c8657c704c947b8472e0587f1efb4ec
Mirek Olšák <mirek@olsak.net>
2025-06-28 17:42:56 +0100

    make "scoped syntax" on one line

--- Qq/Commands.lean
+++ Qq/Commands.lean
@@ -70,10 +70,8 @@ example (a b : Nat) (h : a = b) : True := by
 ```
 See also: `by_elabq`.
 -/
-scoped
-syntax "run_tacq" ident "=>" doSeq : tactic
-scoped
-syntax "run_tacq" doSeq : tactic
+scoped syntax "run_tacq" ident "=>" doSeq : tactic
+scoped syntax "run_tacq" doSeq : tactic
 
 def runTacq (gi : Option Name) (seq : TSyntax `Lean.Parser.Term.doSeq) : TacticM Unit := do
   let goal ← try
