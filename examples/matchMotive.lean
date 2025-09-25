8cc0d1b9f5fafb57aa71898830ca82d73f005a76
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-09-25 14:00:58 -0400

    chore: silence examples, switch to using `lean-action` in CI

--- examples/matchMotive.lean
+++ examples/matchMotive.lean
@@ -7,6 +7,10 @@ elab tk:"showTerm" t:term : term <= expectedType => do
   logAt tk t' (severity := MessageSeverity.information)
   return t'
 
+/--
+info: failed to pretty print expression (use 'set_option pp.rawOnError true' for raw representation)
+-/
+#guard_msgs in
 def provePositive (n : Q(Nat)) : MetaM Q(0 < $n) :=
   showTerm match n with
     | ~q($m + 1) => pure q(Nat.lt_of_le_of_lt (Nat.zero_le _) (Nat.lt_succ_self _))
@@ -16,4 +20,22 @@ def provePositive (n : Q(Nat)) : MetaM Q(0 < $n) :=
         exact Nat.lt_of_le_of_lt (Nat.zero_le _) (Nat.lt_succ_self _))
     | _ => throwError "cannot prove positive: {n}"
 
+/--
+info: (((((Expr.const `Nat.lt_of_le_of_lt []).app
+                  ((((Expr.const `OfNat.ofNat [Level.zero]).app (Expr.const `Nat [])).app
+                        (Expr.lit (Literal.natVal 0))).app
+                    ((Expr.const `instOfNatNat []).app (Expr.lit (Literal.natVal 0))))).app
+              ((((Expr.const `OfNat.ofNat [Level.zero]).app (Expr.const `Nat [])).app
+                    (Expr.lit (Literal.natVal 41))).app
+                ((Expr.const `instOfNatNat []).app (Expr.lit (Literal.natVal 41))))).app
+          ((((Expr.const `OfNat.ofNat [Level.zero]).app (Expr.const `Nat [])).app (Expr.lit (Literal.natVal 42))).app
+            ((Expr.const `instOfNatNat []).app (Expr.lit (Literal.natVal 42))))).app
+      ((Expr.const `Nat.zero_le []).app
+        ((((Expr.const `OfNat.ofNat [Level.zero]).app (Expr.const `Nat [])).app (Expr.lit (Literal.natVal 41))).app
+          ((Expr.const `instOfNatNat []).app (Expr.lit (Literal.natVal 41)))))).app
+  ((Expr.const `Nat.lt_succ_self []).app
+    ((((Expr.const `OfNat.ofNat [Level.zero]).app (Expr.const `Nat [])).app (Expr.lit (Literal.natVal 41))).app
+      ((Expr.const `instOfNatNat []).app (Expr.lit (Literal.natVal 41)))))
+-/
+#guard_msgs in
 #eval provePositive q(42) >>= fun proof => pure proof <* proof.check
