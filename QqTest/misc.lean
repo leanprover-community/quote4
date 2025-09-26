9d03810c3c98704b74d8230249e9bf1e3fdbe582
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2025-09-26 14:18:50 -0400

    rename to QqTest, add drop info to guard_msgs

--- QqTest/misc.lean
+++ QqTest/misc.lean
@@ -6,7 +6,7 @@ def bar2 {α : Q(Sort u)} (a : Q($α)) : Q($a = $a) := q(by simp)
 
 def baz (u : Level) : Type := Q(Sort u)
 
-#guard_msgs(warning, error) in
+#guard_msgs(drop info, warning, error) in
 #eval bar2 q([1,2, 4])
 
 /-- info: q(∀ (x : Nat), x = x + 0) : Q(Prop) -/
@@ -19,11 +19,11 @@ example {α : Q(Type u)} (inst : Q(Inhabited $α)) : Q(∃ x : $α, x = x) :=
 -- TODO: investigate PANIC
 -- example : Q(let x := 5; x = x) := q(by simp)
 
-#guard_msgs(warning, error) in
+#guard_msgs(drop info, warning, error) in
 -- the following also panics if moved into an `example`
 #eval show Q(∀ n : UInt64, n.toFin = n.toFin) from q(fun _ => by simp)
 
 def foo' (n : Nat) : Q(Q($($n) = $($n))) := q(q(by simp))
 
-#guard_msgs(warning, error) in
+#guard_msgs(drop info, warning, error) in
 #eval foo' 3
